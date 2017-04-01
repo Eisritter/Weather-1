eede3f620631aae2f60ca8d036e04b651ee36d02
Eisritter <stalker3101@mail.ru>
2017-04-02 02:52:57 +1000

    Add files via upload

--- app/src/main/java/ru/weather/eisritter/WeatherData.java
+++ app/src/main/java/ru/weather/eisritter/WeatherData.java
@@ -1,6 +1,5 @@
 package ru.weather.eisritter;
 
-//** Вспомогательный класс для работы с API openweathermap.org и скачивания нужных данных
 
 import android.content.Context;
 import android.util.Log;
@@ -11,8 +10,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-//Лишний импорт
-import ru.weather.eisritter.R;
+
 
 /*
     Формирование URL на основании настроек в SharedPreferences
@@ -23,8 +21,7 @@ public class WeatherData {
 
     private static final String OPEN_WEATHER_MAP_API = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
 
-    //Единственный метод класса, который делает запрос на сервер и получает от него данные
-    //Возвращает объект JSON или null
+
     public static JSONObject getJSONData (Context context, String city){
         try{
             URL url = new URL (String.format(OPEN_WEATHER_MAP_API, city));
@@ -34,15 +31,9 @@ public class WeatherData {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
             StringBuffer json = new StringBuffer(1024);
-            //Зачем пустая строка? Почему не просто null, даже IDE подсказывает это reduntant(избыточно)
-            String tmp = "";
-            //Имеет ли смысл добавлять перевод строки?
-            while ((tmp = bufferedReader.readLine()) != null)
-                json.append(tmp).append("\n");
-            bufferedReader.close();
 
             JSONObject data = new JSONObject(json.toString());
-            Log.d("Weather", "data " + data);
+            Log.d("weather", "data " + data);
 
             if (data.getInt("cod") != 200)  return null;
 
